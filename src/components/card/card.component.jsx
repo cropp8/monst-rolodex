4f5f87801898322ef7772ccdd3b9d435a9f0ff1c
Svetlana Samoilenko <svetlana.samoylenko@dalee.ru>
2021-09-12 23:13:39 +0300

    add files for deploy

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,9 @@
+import './card.styles.css';
+
+export const Card = props => (
+  <div className="card-container">
+    <img src={`https://robohash.org/${props.monster.id}?set=set4&size=180x180`} alt="" />
+    <h1>{props.monster.name}</h1>
+    <h2>{props.monster.email}</h2>
+  </div>
+);
